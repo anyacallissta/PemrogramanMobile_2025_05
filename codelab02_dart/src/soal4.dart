eca52f319a484723655471025602b9d669156ade
anyacallissta <vtjhs18@gmail.com>
2025-09-01 18:39:48 +0700

    week 2

---
+++ codelab02_dart/src/soal4.dart
@@ -0,0 +1,13 @@
+void main() {
+// null safety
+  String? alamat;
+  alamat = null;
+  print("Contoh Null Safety");
+  print("Alamat: $alamat");
+
+// late variable
+  late String name;
+  name = "Anya Callissta";
+  print("\nContoh Late Variable");
+  print("Nama: $name");
+}
