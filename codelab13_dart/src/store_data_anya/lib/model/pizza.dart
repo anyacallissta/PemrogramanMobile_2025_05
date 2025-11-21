938dfce0bb0231bf7dfa0577156ae33ffcc766dd
anyacallissta <vtjhs18@gmail.com>
2025-11-21 23:16:58 +0700

    praktikum 3 - menangani error json

--- codelab13_dart/src/store_data_anya/lib/model/pizza.dart
+++ codelab13_dart/src/store_data_anya/lib/model/pizza.dart
@@ -1,3 +1,9 @@
+const keyId = 'id';
+const keyName = 'pizzaName';
+const keyDesc = 'description';
+const keyPrice = 'price';
+const keyImage = 'imageUrl';
+
 class Pizza {
   final int id;
   final String pizzaName;
@@ -11,19 +17,32 @@ class Pizza {
     // description = json['description'],
     // price = json['price'],
     // imageUrl = json['imageUrl'];
-    id = int.tryParse(json['id'].toString()) ?? 0,
-    pizzaName = json['pizzaName'] != null ? json['pizzaName'].toString() : 'No name',
-    description = (json['description'] != null) ? json['description'].toString() : '',
-    price = double.tryParse(json['price'].toString()) ?? 0,
-    imageUrl = json['imageUrl'] ?? '';
-    
+
+    // id = int.tryParse(json['id'].toString()) ?? 0,
+    // pizzaName = json['pizzaName'] != null ? json['pizzaName'].toString() : 'No name',
+    // description = (json['description'] != null) ? json['description'].toString() : '',
+    // price = double.tryParse(json['price'].toString()) ?? 0,
+    // imageUrl = json['imageUrl'] ?? '';
+
+    id = int.tryParse(json[keyId]?.toString() ?? '') ?? 0,
+    pizzaName = json[keyName] != null ? json[keyName].toString() : 'No name',
+    description = (json[keyDesc] != null) ? json[keyDesc].toString() : '',
+    price = double.tryParse(json[keyPrice]?.toString() ?? '') ?? 0,
+    imageUrl = json[keyImage] ?? '';
+
   Map<String, dynamic> toJson() {
     return {
-      'id': id,
-      'pizzaName': pizzaName,
-      'description': description,
-      'price': price,
-      'imageUrl': imageUrl,
+      // 'id': id,
+      // 'pizzaName': pizzaName,
+      // 'description': description,
+      // 'price': price,
+      // 'imageUrl': imageUrl,
+
+      keyId: id,
+      keyName: pizzaName,
+      keyDesc: description,
+      keyPrice: price,
+      keyImage: imageUrl,
     };
   }
 }
