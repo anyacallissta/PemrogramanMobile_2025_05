2a9ebc7667d6493c61c9d7eafbdf9b940f2d46f1
anyacallissta <vtjhs18@gmail.com>
2025-11-21 23:06:04 +0700

    praktikum 2 - handle kompatibilitas data json

--- codelab13_dart/src/store_data_anya/lib/main.dart
+++ codelab13_dart/src/store_data_anya/lib/main.dart
@@ -43,6 +43,7 @@ class _MyHomePageState extends State<MyHomePage> {
           return ListTile(
             title: Text(myPizzas[index].pizzaName),
             subtitle: Text(myPizzas[index].description),
+            trailing: Text('\$${myPizzas[index].price.toStringAsFixed(2)}'),
           );
         },
       )
