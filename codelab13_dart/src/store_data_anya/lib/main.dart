261afbede1ab063b440ce9b07364c584789cda7c
anyacallissta <vtjhs18@gmail.com>
2025-11-22 06:57:47 +0700

    praktikum 4 - shared preferences

--- codelab13_dart/src/store_data_anya/lib/main.dart
+++ codelab13_dart/src/store_data_anya/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import './model/pizza.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,22 +32,41 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String pizzaString = '';
   List<Pizza> myPizzas = [];
+  int appCounter = 0;
   
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
-      appBar: AppBar(title: const Text('JSON by Anya'), backgroundColor: Colors.pink[100]),
-      body: ListView.builder(
-        itemCount: myPizzas.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(myPizzas[index].pizzaName),
-            subtitle: Text(myPizzas[index].description),
-            trailing: Text('\$${myPizzas[index].price.toStringAsFixed(2)}'),
-          );
-        },
-      )
+      // appBar: AppBar(title: const Text('JSON by Anya'), backgroundColor: Colors.pink[100]),
+      // body: ListView.builder(
+      //   itemCount: myPizzas.length,
+      //   itemBuilder: (context, index) {
+      //     return ListTile(
+      //       title: Text(myPizzas[index].pizzaName),
+      //       subtitle: Text(myPizzas[index].description),
+      //       trailing: Text('\$${myPizzas[index].price.toStringAsFixed(2)}'),
+      //     );
+      //   },
+      // )
+
+      appBar: AppBar(title: Text('Shared Preferences by Anya'), backgroundColor: Colors.pink[100]),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              'You have opened this app $appCounter times.',
+            ),
+            ElevatedButton(
+              onPressed: () {
+                deletePreference();
+              },
+              child: const Text('Reset Counter'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -75,14 +95,35 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    readJsonFile().then((value) {
-      setState(() {
-        myPizzas = value;
-      });
-    });
+    // readJsonFile().then((value) {
+    //   setState(() {
+    //     myPizzas = value;
+    //   });
+    // });
+    readAndWritePreferences();
   }
 
   String convertToJSON(List<Pizza> pizzas) {
     return jsonEncode(pizzas.map((pizza) => pizza.toJson()).toList());
   }  
+
+  Future readAndWritePreferences() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    appCounter = prefs.getInt('appCounter') ?? 0;
+    appCounter++;
+    await prefs.setInt('appCounter', appCounter);
+
+    setState(() {
+      appCounter = appCounter;
+    });
+  }
+
+  Future deletePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+
+    setState(() {
+      appCounter = 0;
+    });
+  }
 }
