251e0ebbdb54ff9d69cd9d146be3a99601033b6e
anyacallissta <vtjhs18@gmail.com>
2025-11-28 08:32:54 +0700

    praktikum 1 - membuat layanan mock api

---
+++ codelab14_dart/src/store_data_anya/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import './model/pizza.dart';
+import 'httphelper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter JSON Demo by Anya',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) { 
+    return Scaffold(
+      appBar: AppBar(title: const Text('JSON by Anya'), backgroundColor: Colors.pink[100]),
+      body: FutureBuilder(
+          future: callPizzas(),
+          builder: (BuildContext context, AsyncSnapshot<List<Pizza>>snapshot) {
+          if (snapshot.hasError) {
+            return const Text('Something went wrong');
+          }
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          return ListView.builder(
+            itemCount: (snapshot.data == null) ? 0 : snapshot.data!.length,
+            itemBuilder: (BuildContext context, int position) {
+              return ListTile(
+                title: Text(snapshot.data![position].pizzaName),
+                subtitle: Text(snapshot.data![position].description + ' - € ' + snapshot.data![position].price.toString()),
+              );
+            }
+          );
+        }
+      ),
+    );  
+  }
+
+  Future<List<Pizza>> callPizzas() async {
+    HttpHelper helper = HttpHelper(); 
+    List<Pizza> pizzas = await helper.getPizzaList(); 
+    return pizzas; 
+  } 
+}
